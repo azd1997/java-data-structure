9266837c98ad2faaf01cd626d4ea0a9861bb8be5
azd1997 <374192922@qq.com>
2020-02-13 04:41:08 -0500

    Array-ok

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Java会自动将基本数据类型(int,char,...)转换为包装类型(Integer,Char,...)
         Array<Integer> arr = new Array<Integer>();
-        for (int i = 0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             arr.addLast(i);
         }
         System.out.println(arr);
